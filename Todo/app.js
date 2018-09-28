54cee2f08c80771a45b1338716c9cd06adf6442c
torikraju <torikraju@hotmail.com>
2018-09-28 20:40:30 +0600

    included remove task and clear tasks

--- Todo/app.js
+++ Todo/app.js
@@ -9,6 +9,8 @@ loadEventListners();
 function loadEventListners() {
 
     form.addEventListener('submit', addTask);
+    tasks.addEventListener('click', removeTask);
+    clearTasks.addEventListener('click',removeTasks);
 
 }
 
@@ -16,7 +18,7 @@ function addTask(e) {
     if (inputTask.value === '') {
         alert('Add a task');
     }
-    
+
     tasks.appendChild(createLi(inputTask.value));
     inputTask.value = '';
 
@@ -32,5 +34,17 @@ function createLi(taskvalue) {
     link.innerHTML = '<i class="fa fa-remove"></i>';
     li.appendChild(link);
     return li;
+}
+
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item'))
+        if (confirm('Are you sure'))
+            e.target.parentElement.parentElement.remove();
+}
+
+function removeTasks() {
+    while (tasks.firstChild) {
+        tasks.removeChild(tasks.firstChild);
+    }
 }
 
